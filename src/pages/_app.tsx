86ee655b9c8cbaea5705a5bee4eac7acc246acd8
dreemurgithub <96066405+dreemurgithub@users.noreply.github.com>
2023-04-29 00:04:34 +0700

    create structure of everything

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import "bootstrap/dist/css/bootstrap.min.css";
+import {useEffect} from "react";
+import {SessionProvider} from "next-auth/react"
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({Component, pageProps}: AppProps) {
+    useEffect(() => {
+        require("bootstrap/dist/js/bootstrap.bundle.min.js");
+    }, [])
+    return <SessionProvider session={pageProps.session}>
+        <>
+            <Component {...pageProps} />
+
+        </>
+    </SessionProvider>
 }
---
+++ src/pages/api/auth/[...nextauth].tsx
@@ -0,0 +1,16 @@
+import * as dotenv from 'dotenv'
+import NextAuth from "next-auth"
+import Google from "next-auth/providers/google"
+export const authOptions = {
+    // Configure one or more authentication providers
+    providers: [
+        Google({
+            clientId: process.env.Google_ID,
+            clientSecret: process.env.Google_Secret,
+        }),
+        // ...add more providers here
+    ],
+    secret : process.env.JWT_SECRET
+}
+export default NextAuth(authOptions)
+
