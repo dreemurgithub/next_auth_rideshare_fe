4f7641e5d5f1d60d3e1b70a42c6559687f28510d
dreemurgithub <96066405+dreemurgithub@users.noreply.github.com>
2023-05-06 19:21:13 +0700

    get request from static driver page

--- src/pages/api/request/[id].tsx
+++ src/pages/api/request/[id].tsx
@@ -5,23 +5,23 @@ import path from 'path';
 export default async function handler(req : NextApiRequest,res: NextApiResponse){
     const {id} :{id:string | null}  = req.query as any
     if (req.method==='POST') {
-        const filepath = path.join(process.cwd(),'file','driver','driver.json')
+        const filepath = path.join(process.cwd(),'file','user_driver','user_request.json')
         await fs.writeFile(filepath,JSON.stringify(req.body))
 
         res.status(200).send(req.body)
         return
     }
     if (req.method==='GET') {
-        const filepath = path.join(process.cwd(),'file','driver','driver.json')
+        const filepath = path.join(process.cwd(),'file','user_driver','user_request.json')
         const data_string = await fs.readFile(filepath,"utf8")
         const data_string_object = JSON.parse(data_string)
         let index =0
         for(let i=0;i<data_string_object.length;i++){
             if(data_string_object[i]['id']===id) index=i
         }
-
-        res.status(200).send(data_string_object[index])
-        // res.status(200).send(id)
+        const filter_data = data_string_object.filter((request : any)=>request.id===id)
+        // res.status(200).send(data_string_object[index])
+        res.status(200).send(filter_data)
         return
     }
 }
--- src/pages/driver/[id].tsx
+++ src/pages/driver/[id].tsx
@@ -1,5 +1,6 @@
 import path from "path";
 import {promises as fs} from "fs";
+import {useEffect, useState} from "react";
 
 export async function getStaticPaths(){
 
@@ -25,9 +26,40 @@ export async function getStaticProps(prop_params : {params: {id: string }}){
 }
 
 export default function Driver({driver}:{driver :{
-        vehicle: string , id: string , url:string , price: number , rating: number
-    } }){
+        vehicle: string , id: string , url:string , price: number , rating: number,avatar:string} }){
+    const [request,setrequest] = useState([])
+    useEffect(()=>{
+        fetch(`/api/request/${driver.id}`)
+            .then(res=>res.json())
+            .then(data=>setrequest(data))
+    },[])
     return <>
         <p>{JSON.stringify(driver)}</p>
+        <p>{JSON.stringify(request)}</p>
+        <div>
+
+            <h3>{driver.id}</h3>
+            <h3>{driver.rating}/5</h3>
+            <h3>{driver.price}</h3>
+            <h3>{driver.vehicle}</h3>
+            <img src={driver.url} alt=""/>
+            <img src={driver.avatar} alt=""/>
+        </div>
+        <form>
+            <div className="mb-3">
+                <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div className="mb-3">
+                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                <input type="password" className="form-control" id="exampleInputPassword1"/>
+            </div>
+            <div className="mb-3 form-check">
+                <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+                    <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
     </>
 }
