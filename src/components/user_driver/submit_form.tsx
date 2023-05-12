15c8a4f7c96fde1ebe91b184b5288e440e7ed5c4
dreemurgithub <96066405+dreemurgithub@users.noreply.github.com>
2023-05-12 15:30:13 +0700

    basic style for driver form

--- src/components/user_driver/submit_form.tsx
+++ src/components/user_driver/submit_form.tsx
@@ -2,6 +2,7 @@ import {GoogleMap, MarkerF, useLoadScript} from "@react-google-maps/api";
 import styles from "@/components/trip/history.module.css";
 import {submitRequest} from "@/utils/user_driver/submitRequest";
 import {useEffect} from "react";
+import STYLES from './style.module.css'
 export function Gmap({location}: { location: null | { latitude: number, longitude: number } }) {
     const {isLoaded} = useLoadScript({
         googleMapsApiKey: process.env.GoogleAPI_key as string
@@ -52,33 +53,55 @@ export default function Driver_form({any_session, driver , location}:{
     }
     return <>
         <div>
-            <div className="mb-3">
-                <label htmlFor="exampleInputEmail1" className="form-label">Your email</label>
-                <input type="email" className="form-control" aria-describedby="emailHelp" readOnly={true}
-                       disabled={true} value={any_session ? any_session.user.email : ''}/>
+            <div className={STYLES.driver_form}>
+                <div className="input-group mb-3">
+                    <input type="text" className="form-control" placeholder="Recipient's username"
+                           aria-label="Recipient's username" aria-describedby="basic-addon2"  readOnly={true}
+                           disabled={true} value={any_session ? any_session.user.email : ''}/>
+                    <div className="input-group-append">
+                        <span className="input-group-text" id="basic-addon2">Your email</span>
+                    </div>
+                </div>
+                <div className="input-group mb-3">
+                    <input type="text" className="form-control"
+                           aria-label="Recipient's username" aria-describedby="basic-addon2"
+                           readOnly={true} disabled={true} value={driver.id}/>
+                    <div className="input-group-append">
+                        <span className="input-group-text" id="basic-addon2">Driver</span>
+                    </div>
+                </div>
+                <div className="input-group mb-3">
+                    <input type="text" className="form-control"
+                           aria-label="Recipient's username" aria-describedby="basic-addon2"
+                           readOnly={true} disabled={true} value={'Đà Nẵng'}/>
+                    <div className="input-group-append">
+                        <span className="input-group-text" >City</span>
+                    </div>
+                </div>
+                <div className="input-group mb-3">
+                    <input type="text" className="form-control" placeholder="Recipient's username"
+                           aria-label="Recipient's username" aria-describedby="basic-addon2"
+                           value={driver.price} readOnly={true} disabled={true}/>
+                    <div className="input-group-append">
+                        <span className="input-group-text" id="basic-addon2">Price</span>
+                    </div>
+                </div>
             </div>
-            <div className="mb-3">
-                <label htmlFor="exampleInputPassword1" className="form-label">Driver</label>
-                <input type="text" className="form-control" readOnly={true} disabled={true} value={driver.id}/>
-            </div>
+            <div className={STYLES.driver_form}>
 
-            <div className="mb-3">
-                <label htmlFor="exampleInputPassword1" className="form-label">Price</label>
-                <input type="number" className="form-control" value={driver.price} readOnly={true} disabled={true}/>
+                <div className="input-group mb-3">
+                    <input type="number" className="form-control" id='road' min={0}/>
+                    <div className="input-group-append">
+                        <span className="input-group-text">Trip</span>
+                    </div>
+                </div>
+                <div className="input-group mb-3">
+                    <input type="text" className="form-control" id='address'/>
+                    <div className="input-group-append">
+                        <span className="input-group-text">Address</span>
+                    </div>
+                </div>
             </div>
-            <div className="mb-3">
-                <label htmlFor="exampleInputPassword1" className="form-label">Trip</label>
-                <input type="number" className="form-control" id='road' min={0}/>
-            </div>
-            <div className="mb-3">
-                <label className="form-label">Address</label>
-                <input type="text" className="form-control" id='address'/>
-            </div>
-            <div className="mb-3">
-                <label htmlFor="exampleInputPassword1" className="form-label">City</label>
-                <input type="text" className="form-control" value={'Đà Nẵng'}/>
-            </div>
-
             <Gmap location={location}/>
             <button type="submit" className="btn btn-primary" onClick={user_submit}>Submit</button>
         </div>
--- src/pages/driver/[id].tsx
+++ src/pages/driver/[id].tsx
@@ -2,9 +2,11 @@ import {useEffect, useState} from "react";
 import {useSession} from "next-auth/react"
 import * as dotenv from 'dotenv'
 import {read_File, Add_File} from "@/utils/file_asset";
-import Driver_form,{Gmap} from "@/components/user_driver/submit_form";
+import Driver_form, {Gmap} from "@/components/user_driver/submit_form";
 import {read_state_driver} from "@/utils/driver/read_driver";
 import Request from "@/components/user_driver/request";
+import Styles from './styles.module.css'
+
 export async function getStaticPaths() {
     const data_string = await read_File('driver', 'driver.json')
     const data_string_obj = JSON.parse(data_string)
@@ -29,8 +31,7 @@ export async function getStaticProps(prop_params: { params: { id: string } }) {
 
 export default function Driver({driver}: {
     driver: {
-        vehicle: string, id: string, url: string, price: number, rating: number, avatar: string, email: string
-        ,latitude : number , longitude : number
+        vehicle: string, id: string, url: string, price: number, rating: number, avatar: string, email: string, latitude: number, longitude: number
     }
 }) {
     const {data: session, status} = useSession()
@@ -50,37 +51,74 @@ export default function Driver({driver}: {
     useEffect(() => {
         async function readState_request() {
             const new_state_request = await read_state_driver(driver.id)
-            new_state_request.forEach((el : any)=> {
+            new_state_request.forEach((el: any) => {
                 // if(any_session) el.viewer = any_session.user.email
                 console.log(any_session)
             })
             setrequest(new_state_request)
         }
 
-        setTimeout(readState_request,1000)
+        setTimeout(readState_request, 1000)
 
         getLocation()
     }, [])
 
-    if(any_session!== null) return <>
+    if (any_session !== null) return <>
         <p><b>Driver infor: </b> {JSON.stringify(driver)}</p>
-        <Gmap location={{latitude: driver.latitude, longitude : driver.longitude}} />
         <p><b>The user session: </b> {JSON.stringify(any_session)}</p>
         <p><b>The location: </b> {JSON.stringify(location)}</p>
         <button onClick={getLocation}>Get location</button>
-        <div>
+        <div className={Styles.container}>
+            <div className={Styles.driver}>
+                <img src={driver.avatar} alt="" className={Styles.avatar}/>
+                <h4>{driver.id}</h4>
+                <div className={Styles.small_card}>
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                             className="bi bi-star-fill" viewBox="0 0 16 16">
+                            <path
+                                d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                        </svg>
+                        <span>Rating: <b>{driver.rating}/5</b></span>
+                    </div>
 
-            <h3>{driver.id}</h3>
-            <h3>{driver.rating}/5</h3>
-            <h3>{driver.price}</h3>
-            <h3>{driver.vehicle}</h3>
-            <img src={driver.url} alt=""/>
-            <img src={driver.avatar} alt=""/>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                         className="bi bi-box-arrow-in-right" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd"
+                              d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0v-2z"/>
+                        <path fill-rule="evenodd"
+                              d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
+                    </svg>
+                </div>
+                <div className={Styles.small_card}>
+                    <div>
+
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                             className="bi bi-star-fill" viewBox="0 0 16 16">
+                            <path
+                                d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                        </svg>
+                        <span>Fare: <b>{driver.price}/km</b></span>
+                    </div>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                         className="bi bi-box-arrow-in-right" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd"
+                              d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0v-2z"/>
+                        <path fill-rule="evenodd"
+                              d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
+                    </svg>
+                </div>
+                {/*<h3>{driver.price}</h3>*/}
+                {/*<h3>{driver.vehicle}</h3>*/}
+                <h4 style={{textAlign: 'left'}}>Setting</h4>
+                <Gmap location={{latitude: driver.latitude, longitude: driver.longitude}}/>
+                <img src={driver.url} alt="" style={{maxWidth: '100%'}}/>
+            </div>
+            <Driver_form any_session={any_session} driver={driver} location={location}/>
         </div>
 
-        <Driver_form any_session={any_session} driver={driver} location={location}/>
         <p><b>Request from User: </b> {JSON.stringify(request)}</p>
-        {request.map((el:any,index)=> Request(el,index) )}
+        {request.map((el: any, index) => Request(el, index))}
     </>
     else return null
 }
